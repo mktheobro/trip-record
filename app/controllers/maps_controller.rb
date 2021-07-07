0b165fb5b42c131466998e22869c74f099b9f475
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-07-07 16:55:10 +0900

    ログインできるか確認

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,6 @@
 class MapsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     # このあとで@mapに関するフォームを作るので、Mapインスタンスを作っておきます(でないとエラーになる)
     @map = Map.new
@@ -19,11 +21,21 @@ class MapsController < ApplicationController
     end
   end
 
+  def show
+  
+  end
+
   private
 
   # ストロングパラメーター
   def map_params
     params.require(:map).permit(:address, :latitude, :longitude, :title, :comment)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
