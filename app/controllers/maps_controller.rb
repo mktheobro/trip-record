ebccb22119b616ae52f48c524204ac59730de30b
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-07-09 16:51:36 +0900

    基本構造に修正

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -8,17 +8,24 @@ class MapsController < ApplicationController
   end
 
   def new
-  
+    @point = Point.new
   end
 
   def create
-    @map = Map.new(map_params)
-    if @map.save
-      redirect_to maps_url
+    @point = Point.new(point_params)
+    if @point.save
+      redirect_to root_path
     else
-      @maps = Map.all
-      render 'maps/index'
+      render :new
     end
+
+    # @map = Map.new(map_params)
+    # if @map.save
+    #   redirect_to maps_url
+    # else
+    #   @maps = Map.all
+    #   render 'maps/index'
+    # end
   end
 
   def show
@@ -28,9 +35,10 @@ class MapsController < ApplicationController
   private
 
   # ストロングパラメーター
-  def map_params
-    params.require(:map).permit(:address, :latitude, :longitude, :title, :comment)
-  end
+  
+  # def map_params
+  #   params.require(:map).permit(:address, :latitude, :longitude, :title, :comment)
+  # end
 
   def move_to_index
     unless user_signed_in?
@@ -41,5 +49,10 @@ class MapsController < ApplicationController
   # def message_params
   #   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   # end
+
+  def point_params
+    params.require(:point).permit(:address, :point_comment, :image)
+    # .merge(user_id: current_user.id)
+  end
 
 end
